70d0e36582f73ba3f66fc67bafd541ff4005cc90
Ludmila <lulym329@gmail.com>
2026-01-15 03:27:42 -0300

    monolito

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import { engine } from 'express-handlebars';
+import viewsRouter from './routes/views.routes';
+
+const app = express();
+const PORT = 3000;
+
+// Configuración del motor de plantillas Handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Rutas de vistas
+app.use('/handlebars', viewsRouter);
+
+app.listen(PORT, () => {
+console.log(`Servidor activo en http://localhost:${PORT}`);
+});
